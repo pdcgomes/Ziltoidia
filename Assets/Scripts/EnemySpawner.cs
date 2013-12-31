56620c61bd4d351f51028a4d06c0a63e74e8e51c
Pedro Gomes <pdcgomes@gmail.com>
2013-12-31 18:49:44 +0000

    [ADD] enemy spawner keeps track of living spawned entities and limits their number to a user defined value

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,13 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class EnemySpawner : MonoBehaviour
 {
 	public float spawnStartDelay = 3f;
 	public float spawnInterval = 5f;
-
+    public int maximumNumberOfLivingEntities = 10;
+    
 	public GameObject[] enemies;
+    
+    protected List<GameObject> spawnedEntities = new List<GameObject>();
 	
+    void Awake()
+    {
+    }
 	// Use this for initialization
 	void Start()
 	{
@@ -22,7 +29,25 @@ public class EnemySpawner : MonoBehaviour
 	
 	void SpawnRandomEnemy()
 	{
+        bool canSpawn = (spawnedEntities.Count < maximumNumberOfLivingEntities);
+        if(canSpawn == false) {
+            return;
+        }
+        
 		int index = Random.Range(0, enemies.Length);
-		Instantiate(enemies [index], transform.position, transform.rotation);
+		
+        // Fixme: ensure we spawn at a position where we won't collide with anything (pdcgomes 31.12.2013)
+        Vector3 spawnPosition = new Vector3(transform.position.x + Random.Range(-10f, 10f), 
+                                            transform.position.y + Random.Range(-10f, 10f), 
+                                            transform.position.z);
+        
+        GameObject spawnedEntity = (GameObject)Instantiate(enemies [index], spawnPosition, transform.rotation);
+        spawnedEntity.GetComponent<ShipDamageControl>().OnShipDestroyed += OnSpawnedEntityDestroyed;
+        spawnedEntities.Add(spawnedEntity);
 	}
+    
+    void OnSpawnedEntityDestroyed(GameObject entity)
+    {
+        spawnedEntities.Remove(entity);
+    }
 }
